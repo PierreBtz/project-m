6c888fdb2adb7f786e1a73d8fce50078f40119ba
PierreBtz <PierreBtz@users.noreply.github.com>
2017-01-15 21:12:05 +0100

    Dispose of nginx docker container when closing

--- devtools/start.sh
+++ devtools/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --name project-m-nginx -p 80:80 --net="host" -v $PWD/nginx.conf:/etc/nginx/nginx.conf:ro -d nginx:1.11.8-alpine
+docker run --rm --name project-m-nginx -p 80:80 --net="host" -v $PWD/nginx.conf:/etc/nginx/nginx.conf:ro  nginx:1.11.8-alpine
