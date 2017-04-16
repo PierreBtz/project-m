de0ed35c1114feb0af6f83dbbbe377623caebbb6
PierreBtz <pierrebtz@users.noreply.github.com>
2017-04-16 16:53:16 +0200

    Can now support more up until 5 adults.

--- src/Rsvp.js
+++ src/Rsvp.js
@@ -117,6 +117,9 @@ class Rsvp extends Component {
                                     <option value="0">0</option>
                                     <option value="1">1</option>
                                     <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
                                 </FormControl>
                             </Col>
                             <Col sm={3}>
